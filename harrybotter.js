1b9b469bf47963fc8ee5f1d280123560b22bbb34
HarmaEndless <harma.endless@gmail.com>
2016-12-02 02:37:02 +0100

    Mise à jour, ajout de commentaire

--- harrybotter.js
+++ harrybotter.js
@@ -16,5 +16,20 @@ bot.dialog('/', function (session) {
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 3978, function () {
-	console.log('%s listening to %s', server.name, server.url);
-});
+	var restify = require('restify');
+	var builder = require('botbuilder');
+
+	var connector = new builder.ChatConnector({
+		appId: "Nuitde",
+		appPassword: "l'info"
+	});
+	var bot = new builder.UniversalBot(connector);
+	bot.dialog('/', function (session) {
+		session.send('Expecto Patronum!!!');
+	});
+
+	var server = restify.createServer();
+	server.post('/api/messages', connector.listen());
+	server.listen(process.env.port || 3978, function () {
+		console.log('%s listening to %s', server.name, server.url);
+	});
